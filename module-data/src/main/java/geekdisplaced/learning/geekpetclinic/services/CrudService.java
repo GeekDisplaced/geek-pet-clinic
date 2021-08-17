ea0e63386c03683dd43741e31b58ffd28dd6b726
GeekDisplaced <geek.displaced@icloud.com>
2021-08-17 16:32:58 +0100

    Refactor Service Interfaces to common CRUD interface (closes #11)

---
+++ module-data/src/main/java/geekdisplaced/learning/geekpetclinic/services/CrudService.java
@@ -0,0 +1,16 @@
+package geekdisplaced.learning.geekpetclinic.services;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+
+    Set<T> findAll();
+
+    T findById(ID id);
+
+    T save(T object);
+
+    void delete(T object);
+
+    void deleteById(ID id);
+}
--- module-data/src/main/java/geekdisplaced/learning/geekpetclinic/services/OwnerService.java
+++ module-data/src/main/java/geekdisplaced/learning/geekpetclinic/services/OwnerService.java
@@ -2,15 +2,7 @@ package geekdisplaced.learning.geekpetclinic.services;
 
 import geekdisplaced.learning.geekpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-
-    Owner findById(Long Id);
+public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
