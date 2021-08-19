384f03f6d4d05de8d992ecc87a9f8dd07d64896b
GeekDisplaced <geek.displaced@icloud.com>
2021-08-19 16:01:28 +0100

    "Implement automatic Id generation for the map service"
    (Closes #18)

--- module-web/src/main/java/geekdisplaced/learning/geekpetclinic/web/bootstrap/DataLoader.java
+++ module-web/src/main/java/geekdisplaced/learning/geekpetclinic/web/bootstrap/DataLoader.java
@@ -31,14 +31,12 @@ public class DataLoader implements CommandLineRunner {
         // Manually copied from guru repo.
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
 
@@ -47,14 +45,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
@@ -63,14 +59,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Vets....");
 
         Pet pet1 = new Pet();
-        pet1.setId(1L);
         pet1.setName("Fluffy");
         pet1.setPetType(new PetType("Cat"));
         pet1.setBirthDate(LocalDate.of(2017,5,1));
         petService.save(pet1);
 
         Pet pet2 = new Pet();
-        pet2.setId(2L);
         pet2.setName("Butch");
         pet2.setPetType(new PetType("Dog"));
         pet2.setBirthDate(LocalDate.of(2017,5,1));
