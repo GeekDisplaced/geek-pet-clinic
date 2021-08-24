bb35940a17c27afbd5c9f7f7f28e53c587ea829b
GeekDisplaced <geek.displaced@icloud.com>
2021-08-24 14:50:37 +0100

    Implement Specialty Service Interface and Map implementation version

---
+++ module-data/src/main/java/geekdisplaced/learning/geekpetclinic/services/SpecialtyService.java
@@ -0,0 +1,8 @@
+package geekdisplaced.learning.geekpetclinic.services;
+
+import geekdisplaced.learning.geekpetclinic.model.Specialty;
+import geekdisplaced.learning.geekpetclinic.services.map.AbstractMapService;
+
+public interface SpecialtyService extends CrudService<Specialty, Long>  {
+
+}
