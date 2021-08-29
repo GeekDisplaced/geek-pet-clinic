f8881b0dc34592ebe08fc12c6a9353388574e907
GeekDisplaced <geek.displaced@icloud.com>
2021-08-29 14:29:17 +0100

    Implement Spring Data JPA Pet Service

---
+++ module-data/src/main/java/geekdisplaced/learning/sfgpetclinic/services/sdjpa/SdjpaPetService.java
@@ -0,0 +1,51 @@
+package geekdisplaced.learning.sfgpetclinic.services.sdjpa;
+
+import geekdisplaced.learning.sfgpetclinic.model.Pet;
+import geekdisplaced.learning.sfgpetclinic.repositories.PetRepositiory;
+import geekdisplaced.learning.sfgpetclinic.services.PetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SdjpaPetService extends SdjpaAbstractService implements PetService {
+
+    private final PetRepositiory petRepositiory;
+
+    public SdjpaPetService(PetRepositiory petRepositiory) {
+        this.petRepositiory = petRepositiory;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+
+        Set<Pet> pets = new HashSet<>();
+
+        petRepositiory.findAll().forEach(pets::add);
+
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+
+        return petRepositiory.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+
+        return petRepositiory.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+
+        petRepositiory.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+        petRepositiory.deleteById(id);
+    }
+}
